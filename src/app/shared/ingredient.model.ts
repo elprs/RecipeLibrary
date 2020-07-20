14627d4f6a58e8a8cddaad09e2b7d746fad6e120
elprs <eleni.parisi.sunshine@gmail.com>
2020-07-20 14:02:06 +0300

    ingredient model

---
+++ src/app/shared/ingredient.model.ts
@@ -0,0 +1,17 @@
+
+// export class Ingredient
+// {
+//   public name: string;
+//   public amount: number;
+
+//   constructor(name: string, amount: number){
+//     this.name = name;
+//     this.amount = amount;
+//   }
+// }
+// altrenatively:
+
+export class Ingredient
+{
+  constructor(public name: string, public amount: number) {}
+}
