38a62a2b36a52302feccb2ff989851b7b3fdd419
elprs <eleni.parisi.sunshine@gmail.com>
2020-07-20 12:00:58 +0300

    generated main components and folder structure

---
+++ src/app/header/header.component.ts
@@ -0,0 +1,12 @@
+import { Component } from "@angular/core";
+
+
+
+@Component({
+  // pass a js object here to configute this decorator
+  selector: 'app-header',
+  templateUrl: './header.component.html'
+})// a component is just a ts class, needs an identifier and a decorator
+export class HeaderComponent{
+
+}
