d63c60bf3d1ecd94e14827cdbbc5cd68680be508
elprs <eleni.parisi.sunshine@gmail.com>
2020-07-20 15:40:43 +0300

    adding navigation and event binding with ngIf

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component } from "@angular/core";
+import { Component, EventEmitter, Output } from "@angular/core";
 
 
 
@@ -10,5 +10,12 @@ import { Component } from "@angular/core";
 })// a component is just a ts class, needs an identifier and a decorator
 export class HeaderComponent{
   collapsed = true;
+  @Output() featureSelected = new EventEmitter<string>();
+  // @output makes it listenable from outside this component
+
+  onSelect(feature: string){
+    this.featureSelected.emit(feature);
+    // use this property to emit an event whenever we click the buttons
+  }
 
 }
