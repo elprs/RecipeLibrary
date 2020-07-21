24322241cdc75d1aa133315de4ab33a5610a7f94
elprs <eleni.parisi.sunshine@gmail.com>
2020-07-21 10:28:37 +0300

    add ingrediend to the shopping list

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -8,9 +8,14 @@ import { Component, OnInit, ElementRef, ViewChild, EventEmitter, Output } from '
 })
 export class ShoppingEditComponent implements OnInit {
 
+  // I could have used the local references inthe html file to pass the values
+  // but i will use the @ViewChild approach
+  // so the nameInputRef gets a value by using the @ViewChild
   @ViewChild('nameInput') nameInputRef: ElementRef;
   @ViewChild('amountInput') amountInputRef: ElementRef;
   // ingredientAdded = new EventEmitter<{name: string, amount: string}>();
+  // it accepts in the <>  a type definition , meaning a JS object. Since we have
+  // already the type we use straight the Ingredient model:
   @Output() ingredientAdded = new EventEmitter<Ingredient>();
 
   constructor() { }
@@ -23,6 +28,7 @@ export class ShoppingEditComponent implements OnInit {
     const ingredientName = this.nameInputRef.nativeElement.value;
     const ingredientAmount = this.amountInputRef.nativeElement.value;
     const newIngredient = new Ingredient(ingredientName, ingredientAmount );
+    // I emit the new event and pass the newIngredient as data
     this.ingredientAdded.emit(newIngredient);
   }
 }
--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -4,22 +4,20 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-shopping-list',
   templateUrl: './shopping-list.component.html',
-  styleUrls: ['./shopping-list.component.css']
+  styleUrls: ['./shopping-list.component.css'],
 })
 export class ShoppingListComponent implements OnInit {
-   ingredients: Ingredient[] = [
-     new Ingredient("apples", 5),
-     new Ingredient("tomatoes", 25),
-     new Ingredient("kiwis", 5)
-   ];
+  ingredients: Ingredient[] = [
+    new Ingredient('apples', 5),
+    new Ingredient('tomatoes', 25),
+    new Ingredient('kiwis', 5),
+  ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
-  onIngredientAdded(ingredient : Ingredient){
+  onIngredientAdded(ingredient: Ingredient) {
     this.ingredients.push(ingredient);
-
   }
 }
