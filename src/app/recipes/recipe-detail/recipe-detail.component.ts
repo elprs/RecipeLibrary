f887eba1a450eb78e6ea58b934cea82d48963584
elprs <eleni.parisi.sunshine@gmail.com>
2020-07-21 09:55:52 +0300

    event and property binding

--- src/app/recipes/recipe-detail/recipe-detail.component.ts
+++ src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Recipe } from '../recipe.model';
+
 
 @Component({
   selector: 'app-recipe-detail',
@@ -6,7 +8,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-detail.component.css']
 })
 export class RecipeDetailComponent implements OnInit {
-
+  @Input() recipe: Recipe; // we add this property in order to get the details data to the
+                           // recipes component
   constructor() { }
 
   ngOnInit(): void {
