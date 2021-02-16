ba17b222d20b2094c5901179d9028a45bcb62873
elprs <parisi.code@gmail.com>
2021-02-16 16:28:01 +0200

    create user entity

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,7 +1,8 @@
 import { elementEventFullName } from '@angular/compiler/src/view_compiler/view_compiler';
 import { Component } from '@angular/core';
 import { NgForm } from '@angular/forms';
-import { AuthService } from './auth.service';
+import { Observable } from 'rxjs';
+import { AuthService, AuthResponseData } from './auth.service';
 
 @Component({
   selector: 'app-auth',
@@ -25,26 +26,31 @@ export class AuthComponent {
     const email = form.value.email;
     const password = form.value.password;
 
+    let authObs = new Observable<AuthResponseData>();
+
     // here is before the request, thus we can put  the loading spinner to start by setting true the isLoading
     this.isLoading = true;
     if (this.isLoginMode) {
-      //...
+      //Log in the user
+      authObs = this.authService.logIn(email, password);
     } else {
       //subscribe the user
-      this.authService.signup(email, password, true).subscribe(
-        (resData) => {
-          console.log(resData);
-          this.isLoading = false;
-        },
-        (errorMessage) => { // an observable with the error message returning from the authService dince we subscribed
-          //So we know that we get the error message here as a data point, but more importanly we can assign it to the this.error 
-          console.log(errorMessage);
-          this.error = errorMessage;
-          this.isLoading = false;
-        }
-      );
+      authObs = this.authService.signup(email, password, true);
     }
 
+    authObs.subscribe(
+      (resData) => {
+        console.log(resData);
+        this.isLoading = false;
+      },
+      (errorMessage) => {
+        // an observable with the error message returning from the authService dince we subscribed
+        //So we know that we get the error message here as a data point, but more importanly we can assign it to the this.error
+        console.log(errorMessage);
+        this.error = errorMessage;
+        this.isLoading = false;
+      }
+    );
     form.reset();
   }
 }
--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,56 +1,104 @@
-import { HttpClient } from "@angular/common/http";
-import { Injectable } from "@angular/core";
-import { catchError } from "rxjs/operators";
-import{throwError} from 'rxjs';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { catchError, tap } from 'rxjs/operators';
+import { Observable, Subject, throwError } from 'rxjs';
+import { User } from './user.model';
 
-
-
-
-interface AuthResponseData{
- kind: string;
- idToken: string;
- email: string;
- refreshToken: string;
- expiresIn: string;
- localId: string;
-
+export interface AuthResponseData {
+  kind: string;
+  idToken: string;
+  email: string;
+  refreshToken: string;
+  expiresIn: string;
+  localId: string;
+  registered?: boolean;
 }
+let authObs = new Observable<AuthResponseData>();
+
+@Injectable({ providedIn: 'root' })
+export class AuthService {
+  user = new Subject<User>();
 
 
-@Injectable({providedIn: 'root'})
-export class AuthService
-{
-  constructor(
-    private http: HttpClient){
+  constructor(private http: HttpClient) {}
 
-  }
 
-  signup(email: string, password: string, returnSecureToken: boolean){
+
+  signup(email: string, password: string, returnSecureToken: boolean) {
+    return this.http
+      .post<AuthResponseData>(
+        'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyBvfEjBFiVlPGSCst5I-G5YvzwBQxzScSg',
+        {
+          email: email,
+          password: password,
+          returnSecureToken: true,
+        }
+      )
+      .pipe(
+        catchError(this.handleError),
+        tap(resData => {
+          this.handleAuthentication(
+            resData.email,
+            resData.localId,
+            resData.idToken,
+            +resData.expiresIn
+            );
+        })
+      )
+
+  };
+
+  logIn(email: string, password: string){
     return this.http.post<AuthResponseData>(
-      'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyBvfEjBFiVlPGSCst5I-G5YvzwBQxzScSg',
+      'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyBvfEjBFiVlPGSCst5I-G5YvzwBQxzScSg',
       {
         email: email,
         password: password,
         returnSecureToken: true
       }
-
-    ).pipe(catchError(errorRes => {
-      let errorMessage = 'An unknown error occurred.'; // initiate it in case we cannot identify any error
-      if(!errorRes.error || !errorRes.error.error){ // the switch will fail in the case that we have errow with different format, eg a network error.  thus we check with the if
-        //we throw an observable that in the end wraps that message
-        return throwError(errorMessage);
-      }
-      else{
-        switch(errorRes.error.error.message){
-          case 'EMAIL_EXISTS':
-            errorMessage= 'This email exists.';
-
-        }
-        return throwError(errorMessage);
-      }
-
-
-    }));
+    ).pipe(
+      catchError(this.handleError)
+    );
 
   }
+
+  private handleError(errorRes: HttpErrorResponse){
+    let errorMessage = 'An unknown error occurred.'; // initiate it in case we cannot identify any error
+    if (!errorRes.error || !errorRes.error.error) {
+      // the switch will fail in the case that we have errow with different format, eg a network error.  thus we check with the if
+      //we throw an observable that in the end wraps that message
+      return throwError(errorMessage);
+    } else {
+      switch (errorRes.error.error.message) {
+        case 'EMAIL_EXISTS':
+          errorMessage = 'This email exists.';
+          break;
+          case 'EMAIL_NOT_FOUND':
+            errorMessage = 'This email does not exist';
+            break;
+            case 'INVALID_PASSWORD':
+            errorMessage = 'This password is invalid';
+            break
+      }
+      return throwError(errorMessage);
+    }
+  };
+
+  private handleAuthentication(
+    email: string,
+    userId: string,
+    token: string,
+    expiresIn: number)
+    {
+    const expirationDate = new Date(new Date().getTime() + +expiresIn * 1000);
+    const user = new User(
+      email,
+      userId,
+      token,
+      expirationDate
+    );
+    this.user.next(user);
+  }
+
+
 }
